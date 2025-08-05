d1975a9c44dea445bb7ab31283c5d4246b5a9718
ppsmrt <124156458+ppsmrt@users.noreply.github.com>
2025-08-05 20:20:36 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1,26 +1,6 @@
 export const users = [
-  {
-    username: 'member1',
-    passwordHash: '4b49e5a9716ee22d6a352c3f4b7b9e1a196a8b02c185d2d842c1a1f1fb5e7e29',
-    displayName: 'Alice',
-    isAdmin: true
-  },
-  {
-    username: 'member2',
-    passwordHash: '6e0465cf1d1f21c54620528c7b97c382e9617bc9b3f404bd574f881a82ff4f29',
-    displayName: 'Bob',
-    isAdmin: false
-  },
-  {
-    username: 'member3',
-    passwordHash: 'bb73ad1388b55a1bb01557e1e2a0f6e991b5db4df3ef12e1d702d9ac5a0b405d',
-    displayName: 'Charlie',
-    isAdmin: false
-  },
-  {
-    username: 'member4',
-    passwordHash: 'b86d03c1e7ab0fcb64e2cc9ac0b2bff91e59e9e16272d1b96c5e2e40d6d2c6e7',
-    displayName: 'Diana',
-    isAdmin: false
-  }
+  { username: 'member1', passwordHash: '4b49e5a9716ee22d6a352c3f4b7b9e1a196a8b02c185d2d842c1a1f1fb5e7e29', displayName: 'Alice', isAdmin: true },
+  { username: 'member2', passwordHash: '6e0465cf1d1f21c54620528c7b97c382e9617bc9b3f404bd574f881a82ff4f29', displayName: 'Bob', isAdmin: false },
+  { username: 'member3', passwordHash: 'bb73ad1388b55a1bb01557e1e2a0f6e991b5db4df3ef12e1d702d9ac5a0b405d', displayName: 'Charlie', isAdmin: false },
+  { username: 'member4', passwordHash: 'b86d03c1e7ab0fcb64e2cc9ac0b2bff91e59e9e16272d1b96c5e2e40d6d2c6e7', displayName: 'Diana', isAdmin: false },
 ];
